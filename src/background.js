143d0949448b0545ecf9aa9d5c57c841633933be
Ben <benfinch04@gmail.com>
2023-02-14 08:35:17 -0700

    added ChatGPT Prompt Genius attribution

--- src/background.js
+++ src/background.js
@@ -1,3 +1,11 @@
+/* Based off of ChatGPT Prompt Genius - https://github.com/benf2004/ChatGPT-Prompt-Genius
+@benf2004 - https://github.com/benf2004/ - Creator
+@aivct - https://github.com/aivct/ - Major Contributor
+@itsmohmans - https://github.com/itsmohmans - Contributor
+@Abhishek7Tech - https://github.com/Abhishek7Tech - Contributor
+Code used throughout many/most files
+*/
+
 let settings;
 // Listen for a click on the browser action
 chrome.action.onClicked.addListener(function(tab) {
